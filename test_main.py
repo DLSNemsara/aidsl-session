d251129680570ea38464568014201517b5fb52c2
pasandevin <jayawardene.pasandevin@gmail.com>
2024-09-13 13:24:43 +0530

    Changed the test

--- test_main.py
+++ test_main.py
@@ -6,4 +6,3 @@ def test_read_root_returns_hello_world():
     client = TestClient(app)
     response = client.get("/")
     assert response.status_code == 200
-    assert response.json() == {"Hello": "World"}
