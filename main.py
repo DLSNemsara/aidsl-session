4e69a483d810283aa018e82f1f3ba16dcca88b9e
pasandevin <jayawardene.pasandevin@gmail.com>
2024-09-13 20:38:00 +0530

    test commit

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ def read_root():
 @app.post("/generate_openapi")
 async def generate_openapi(request: UserInstruction):
     response = co.chat(
-        message=f"""Generate OpenAPI specs for the following instruction. 
+        message=f"""Generate OpenAPI specs for the following instruction.. 
         Just putout the spec in markdown yaml. don't give any other comments: {request.user_instruction}"""
     )
 
